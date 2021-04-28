2d54591edeec99acc689da539d04005d67719643
Alan Mimms <alanmimms@gmail.com>
2021-04-28 16:19:19 -0700

    Read seems working

--- spi0.lua
+++ spi0.lua
@@ -12,6 +12,11 @@ local ENTER_4B_MODE, EXIT_4B_MODE = 0xB7, 0xE9
 local READ_4B = 0x13
 
 
+local DeviceSize = 64*1024*1024
+local ChunkSize = 2048
+local MB = 1*1024*1024
+
+
 -- Dump the specified string in hex and ASCII with address offsets in
 -- blocks of 16 as has been done since the Big Bang.
 function hexDump(s)
@@ -50,34 +55,92 @@ Usage:
   spi0 {read,verify,write,erase} device [file]
 
 Read, or read and verify, or erase and write, or simply erase the flash device on device (e.g., /dev/spidev0.0).
-The `file` parameter is used for verfiy and write operations.
+The `file` parameter is used for read, verify, and write operations.
 
-]], msg)
+]], msg, '\n')
   os.exit(false, true)
 end
 
 
+function openChecked(f, mode)
+  local op = 'reading'
+  if mode:sub(1, 1) == 'w' then op = 'writing' end
+  local f = io.open(f, mode)
+  if not f then usage('Cannot open "' .. f .. ' for ' .. op) end
+  return f
+end
 
-if #args < 3 or #args > 4 then usage('Must have three or four arguments') end
 
-local op = args[2]
-local device = args[3]
-local file = args[4]
+
+function doRead(devFD, file)
+  local f = openChecked(file, "wb")
+  local lastMB = -1
+  local addr
+
+  print("Reading device and dumping to " .. file)
+  io.stdout:setvbuf('no')
+
+  for addr = 0, DeviceSize - 1, ChunkSize do
+
+    if addr // MB ~= lastMB then
+      local eol
+      lastMB = addr // MB
+
+      if (lastMB % 16 == 15) then
+        eol = '\n'
+      else
+        eol = ''
+      end
+      
+      io.write(string.format("%3dMB%s", lastMB, eol))
+    end
+
+    local readBuf = SPIOPS.doCommand(devFD, string.pack(">BI4", READ_4B, addr), ChunkSize)
+    f:write(readBuf)
+  end
+
+  f:close()
+  io.write('\n')
+end
+
+
+function doWrite(devFD, file)
+  print('Write is not yet implemented')
+end
+
+
+function doVerify(devFD, file)
+  print('Verify is not yet implemented')
+end
+
+
+function doErase(devFD)
+  print('Erase is not yet implemented')
+end
+
+
+
+
+if #arg < 2 or #arg > 3 then usage('Must have two or three arguments') end
+
+local op = arg[1]
+local device = arg[2]
+local file = arg[3]
 
 if op == 'read' then
-  if #args ~= 3 then usage('Write requires three arguments') end
+  if #arg ~= 3 then usage('Read requires three arguments') end
 elseif op == 'write' then
-  if #args ~= 4 then usage('Write requires four arguments') end
+  if #arg ~= 3 then usage('Write requires three arguments') end
 elseif op == 'erase' then
-  if #args ~= 3 then usage('Write requires three arguments') end
+  if #arg ~= 2 then usage('Erase requires two arguments') end
 elseif op == 'verify' then
-  if #args ~= 4 then usage('Verify requires four arguments') end
+  if #arg ~= 3 then usage('Verify requires three arguments') end
 else
   usage('Unknown operation "' .. op .. '". Must use read, write, erase, or verify.')
 end
 
 local devFD = SPIOPS.doOpen(device)
-if not devFD then usage('Error opening device "' .. device .. '" for SPI operations.')
+if not devFD then usage('Error opening device "' .. device .. '" for SPI operations.') end
 
 
 SPIOPS.setMode(devFD, 0)
@@ -85,19 +148,56 @@ SPIOPS.setBPW(devFD, 8)
 SPIOPS.setSpeed(devFD, SPISpeed)
 
 
+local manufacturers = {
+  [0x20] = 'Micron',
+}
+
+local types = {
+  [0xBA] = '3V',
+  [0xBB] = '1.8V',
+}
+
+local capacities = {
+  [0x22] = 2048 // 8,
+  [0x21] = 1024 // 8,
+  [0x20] = 512 // 8,
+  [0x19] = 256 // 8,
+  [0x18] = 128 // 8,
+  [0x17] = 64 // 8,
+}
+
 local readIDBuf = SPIOPS.doCommand(devFD, string.pack(">B", READ_ID), 20)
-print(string.format("Read ID: Manufacturer=%02X type=%02X capacity=%02X", readIDBuf.byte(1, 2, 3));
+local devMfg, devType, devCap = readIDBuf:byte(1, 3)
+local devMfgString = manufacturers[devMfg] or '?'
+local devTypeString = types[devType] or '?'
+local devCapString = capacities[devCap] or '?'
+print(string.format("Read ID: Manufacturer=%02X(%s) type=%02X(%s) capacity=%02X(%sMB)",
+    devMfg, devMfgString, devType, devTypeString, devCap, devCapString))
 hexDump(readIDBuf)
 
-local sfdpBuf = SPIOPS.doCommand(devFD, JEDEC.SFDPCommand, 512)
-print("SFDP: length", #sfdpBuf);
-hexDump(sfdpBuf)
+if (readIDBuf:byte(5) & 0x03) ~= 0x00 then
+  usage('Extended Device Data says sector size is not uniform 64K which is all this program supports right now.')
+end
+
+
+if false then
+  local sfdpBuf = SPIOPS.doCommand(devFD, JEDEC.SFDPCommand, 512)
+  print("SFDP: length", #sfdpBuf);
+  hexDump(sfdpBuf)
+end
 
 
 SPIOPS.doCommand(devFD, ENTER_4B_MODE, 0)
 
+if op == 'read' then doRead(devFD, file)
+elseif op == 'write' then doWrite(devFD, file)
+elseif op == 'verify' then doVerify(devFD, file)
+elseif op == 'erase' then doErase(devFD)
+  usage('Unknown "op" value "' .. op .. '"')
+end
 
-if (false) then
+
+if false then
 local n
 local count = 4000
 for n=0,9 do
@@ -114,7 +214,7 @@ end
 end
 
 
-if (false) then
+if false then
   local DeviceSize = 64*1024*1024
   local ChunkSize = 2048
   local MB = 1*1024*1024
@@ -148,5 +248,4 @@ if (false) then
   f:close()
 end
 
-io.write('\n')
 SPIOPS.doClose(devFD)
